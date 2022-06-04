61c4eb243d5e9b0a71d97f73dcfbb7382b07d3a1
rodolfo <rodolfo_mno@hotmail.com>
2022-06-04 20:54:04 -0300

    feat: implements vehicle interface

---
+++ src/interfaces/VehicleInterface.ts
@@ -0,0 +1,11 @@
+import { z as zod } from 'zod';
+
+export const VehicleSchema = zod.object({
+  model: zod.string().min(3),
+  year: zod.number().min(1900).max(2022),
+  color: zod.string().min(3),
+  status: zod.boolean().optional(),
+  buyValue: zod.number().int(),
+});
+
+export type Vehicle = zod.infer<typeof VehicleSchema>;
