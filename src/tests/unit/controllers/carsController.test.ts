ec232d218ccfbb5091aedad0f194983169c82d5a
rodolfo <rodolfo_mno@hotmail.com>
2022-06-06 10:52:05 -0300

    test: add more test cases for controller

--- src/tests/unit/controllers/carsController.test.ts
+++ src/tests/unit/controllers/carsController.test.ts
@@ -1,11 +1,10 @@
 import chai from 'chai';
 import { Response } from 'superagent';
-import App from '../../../app';
+import { app } from '../../../app';
 import chaiHttp = require('chai-http');
 
 
 chai.use(chaiHttp);
-const app = new App();
 
 const { expect } = chai;
 
@@ -15,10 +14,16 @@ describe('Test case for /cars get endpoint', () => {
         expect(res).to.have.status(200);
     });
   });
-  it('endpoint return http status code 404', () => {
-    chai.request(app).get('/cars/id?=999999').end((res: Response) => {
+
+  it('endpoint get  return http status code 404', () => {
+    chai.request(app).get('/cars/123456789012345678901234').end((res: Response) => {
+        console.log(res);
         expect(res).to.have.status(404);
     });
   });
-
+  it('endpoint delete return http status code 404', () => {
+     chai.request(app).delete('/cars/123456789012345678901234').end((res: Response) => {
+        expect(res).to.have.status(404);
+    });
+  });
  });
