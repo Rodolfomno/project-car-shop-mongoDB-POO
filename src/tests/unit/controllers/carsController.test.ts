1b07ec30e934587d458e743318fdef2caa8dc634
rodolfo <rodolfo_mno@hotmail.com>
2022-06-06 03:37:35 -0300

    fix: dont know anymore

--- src/tests/unit/controllers/carsController.test.ts
+++ src/tests/unit/controllers/carsController.test.ts
@@ -1,17 +1,24 @@
 import chai from 'chai';
 import { Response } from 'superagent';
-import app from '../../../app';
+import App from '../../../app';
 import chaiHttp = require('chai-http');
 
 
 chai.use(chaiHttp);
+const app = new App();
 
 const { expect } = chai;
 
 describe('Test case for /cars get endpoint', () => {
   it('endpoint return http status code 200', () => {
-    chai.request(new app()).get('/cars').end((res: Response) => {
+    chai.request(app).get('/cars').end((res: Response) => {
         expect(res).to.have.status(200);
     });
   });
+  it('endpoint return http status code 404', () => {
+    chai.request(app).get('/cars/id?=999999').end((res: Response) => {
+        expect(res).to.have.status(404);
+    });
+  });
+
  });
--- src/tests/unit/models/carsModel.test.ts
+++ src/tests/unit/models/carsModel.test.ts
@@ -1,2 +1,44 @@
-// import { expect } from 'chai';
-// import mongoose from 'mongoose';
+import Sinon from 'sinon';
+import chai from 'chai';
+import chaiHttp = require('chai-http');
+import CarModel from '../../../models/CarsModel';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+export const validCar = {
+  _id: '999999999',
+  model: 'Uno da Escada',
+  year: 1963,
+  color: 'red',
+  buyValue: 3500,
+  seatsQty: 2,
+  doorsQty: 2
+};
+
+describe('Test case car Model', () => {
+  const model = new CarModel();
+
+  describe('verify when create a car', () => {
+    before(async () => {
+      Sinon.stub(model, 'create').resolves(validCar as any)
+    });
+    after(() => {
+      Sinon.restore();
+    });
+    it('verify return', async () => {
+      const result = await model.create(validCar);
+      expect(result).to.be.an('object')
+    });
+    it('verify property id', async () => {
+      const result = await model.create(validCar);
+      expect(result).to.have.property('_id');
+
+    });
+    it('return right value', async () => {
+      const result = await model.create(validCar);
+      expect(result).to.be.deep.equal(validCar);
+    });
+  });
+});
